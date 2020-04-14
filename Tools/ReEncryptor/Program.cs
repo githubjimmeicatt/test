c81b8dcb3a4f447cac4c8e9994622980e61f93b8
mark stoker <mark@icatt.nl>
2020-04-14 12:51:16 +0200

    tool toegeovoegd doet nu alleen dat ophalen en decrypten

---
+++ Tools/ReEncryptor/Program.cs
@@ -0,0 +1,76 @@
+﻿using Icatt.Logging.DataAccess;
+using ReEncryptor.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReEncryptor
+{
+
+
+
+    /// <summary>
+    /// fetches all encrypted records
+    /// decrypts with old key
+    /// encrypts with new key
+    /// stores values back in database
+    /// </summary>
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("heb je dewebsite uitgezet? (j/n) ");
+            var keyInfoWebsiteUit = Console.ReadKey();
+            if (keyInfoWebsiteUit.KeyChar != 'j')
+            {
+                return;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"je gaat deze omgeving opnieuw versleutelen: {Settings.Default.Omgeving}. Ok? (j/n) ");
+            var keyInfoOmgeving = Console.ReadKey();
+            if (keyInfoOmgeving.KeyChar != 'j')
+            {
+                return;
+            }
+            Console.WriteLine();
+
+            var x = new UpdateAuditDatabase();
+            var cert = new Cert().FindCertificateByThumbprint(Settings.Default.KeyVaultCertificateThumbprint);
+
+            x.Update(cert);
+       
+
+        }
+ 
+
+
+    }
+
+    public class Cert
+    {
+        public X509Certificate2 FindCertificateByThumbprint(string findValue)
+        {
+            X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection col = store.Certificates.Find(X509FindType.FindByThumbprint, findValue, false);
+                if (col == null || col.Count == 0)
+                    return null;
+                return col[0];
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+    }
+}
---
+++ Tools/ReEncryptor/UpdateAuditDatabase.cs
@@ -0,0 +1,64 @@
+﻿using Icatt.Azure.Access;
+using Icatt.Logging.DataAccess;
+using Icatt.Security.Engine.Cryptographer.Interface;
+using Icatt.Security.Engine.Cryptographer.Service;
+using ReEncryptor.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReEncryptor
+{
+
+
+
+
+    public class UpdateAuditDatabase
+    {
+
+        private LoggingRepositoryFactory _factory;
+
+        private LoggingRepositoryFactory Factory
+        {
+            get
+            {
+                return _factory ??
+                       (_factory =
+                           new LoggingRepositoryFactory("name=" + Settings.Default.Database));
+            }
+        }
+
+        private ICryptographer _cryptoEngine;
+
+        private ICryptographer CryptoEngine => _cryptoEngine ?? (_cryptoEngine = new CryptographerEngine<object>(null, null));
+
+
+
+        public void Update(System.Security.Cryptography.X509Certificates.X509Certificate2 cert)
+        {
+            var secret = Settings.Default.KeyVaultAuditSecrectOld; //path to the secret
+            var applicationId = Settings.Default.KeyVaultApplicationId; //applicatie id van de app registration
+
+            var keyVault = new KeyVault(cert, applicationId);
+            byte[] key = keyVault.GetSecret(secret);
+
+            var cipherName = "Aes256With16ByteIvPrefix";
+
+
+           
+            using (var rep = Factory.Create())
+            {
+                var list = rep.GetLogEntries(new LogEntryFilter()).ToList();
+                foreach (var item in list)
+                {
+                    var detailsDecrypted = CryptoEngine.Decrypt(cipherName, key, item.DetailsEncrypted);
+                    Console.WriteLine(Encoding.UTF8.GetString(detailsDecrypted));
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
