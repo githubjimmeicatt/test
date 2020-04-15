13963bbecd19174aff6cfe523634a7e56a02a91c
Felix Cornelissen <felix@icatt.nl>
2020-04-15 16:44:30 +0200

    unit tests

---
+++ Klantportaal/Test/Sphdhv.KlantPortaal.Manager.MijnPensioen.ServiceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sphdhv.KlantPortaal.Manager.MijnPensioen.ServiceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sphdhv.KlantPortaal.Manager.MijnPensioen.ServiceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e53a139-a8d4-42a2-9d79-d474837cf311")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
