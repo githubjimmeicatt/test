ef800e058002b868519757dd02b98912e3660104
Felix Cornelissen <felix@icatt.nl>
2020-04-15 17:17:09 +0200

    more tests

---
+++ Klantportaal/Test/Sphdhv.KlantPortaal.Manager.Authentication.ServiceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sphdhv.KlantPortaal.Manager.Authentication.ServiceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sphdhv.KlantPortaal.Manager.Authentication.ServiceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("541079e6-3c7c-42ea-abae-843940a2d3fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
