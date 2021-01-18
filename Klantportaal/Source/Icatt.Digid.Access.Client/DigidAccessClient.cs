09f256cfc0d4f86ae582f8a726e2dc7f349eb659
Pascal van Oostenbrugge <pascal@icatt.nl>
2021-01-18 19:32:35 +0100

    revert to try catch (grrr)

--- Klantportaal/Source/Icatt.Digid.Access.Client/DigidAccessClient.cs
+++ Klantportaal/Source/Icatt.Digid.Access.Client/DigidAccessClient.cs
@@ -240,12 +240,30 @@ namespace Icatt.Digid.Access.Client
             {
                 store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadOnly);
 
-                var certId = subjectDistinguishedName;
+
+                //het tophalen van het certificaat op basis van de waarde van een release variabele werkt om een of andere reden niet meer! tech dept.
+                //daarom voorlopig de subjects hard in de code. 
+                //  var find = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, true);
+
+                var certId = "CN=mijn.pensioenfondshaskoningdhv.nl, O=Stichting Pensioenfonds HaskoningDHV, L=Amersfoort, C=NL";
 
                 var find = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, certId, true);
 
                 var certEnum = find.OfType<X509Certificate2>();
-                       
+
+                //als je niet bij het prod certificaat kan zit je blijkbaar op accept of dev.
+                //het prod certificaat vind je wel op accept (want zelfde server als live), maar je hebt er geen recht op. dat wordt getest door deze try catch
+                try { var cer = certEnum.FirstOrDefault().PrivateKey; }
+                catch (System.Security.Cryptography.CryptographicException e)
+                {
+
+                    var acceptCertId = "CN=mijn.accept.pensioenfondshaskoningdhv.nl, O=Stichting Pensioenfonds HaskoningDHV, L=Amersfoort, C=NL";
+
+                    find = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, acceptCertId, true);
+
+                    certEnum = find.OfType<X509Certificate2>();
+                }
+
 
 
                 //Kies het langst geldige certificaat dat nu geldig is.
