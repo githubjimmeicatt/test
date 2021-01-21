a458ca5359f7fc4364372bec02f1e3c96ad46028
Pascal van Oostenbrugge <pascal@icatt.nl>
2021-01-21 12:57:49 +0100

    wip

---
+++ Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/vue.config.js
@@ -0,0 +1,14 @@
+﻿module.exports = {
+          outputDir: 'client-app/dist',
+          filenameHashing: false,
+          configureWebpack: {
+          optimization: {
+                splitChunks: false
+            },
+            resolve: {
+                alias: {
+                    'vue$': 'vue/dist/vue.esm.js'
+                }
+            }
+        },
+    }
