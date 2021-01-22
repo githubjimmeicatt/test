c9486b3e6b209e8669494ca1db8ee9b38e6b6bfd
Pascal van Oostenbrugge <pascal@icatt.nl>
2021-01-22 15:09:40 +0100

    vue frontend via webconfig rules (outcommented)

--- Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/client-app/src/views/Home.vue
+++ Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/client-app/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your SPH klantportaal App"/>
-  </div>
+    <div id="phMijnPensioenSpa">
+        <h1 class="u-visuallyhidden">Mijn SPHDHV</h1><div class="page-ie">
+            <div class="page">
+                <!-- Main --><div class="main"><!-- Backlink --><div class="u-bgcolor-dark-0"><div class="u-container"><a class="backlink" href="https://www.pensioenfondshaskoningdhv.nl"><svg><use xlink:href="/Portals/mijn/Skins/klantportaal/assets/icons/output/icons.symbol.svg#icn-arrow-left"></use></svg>pensioenfondshaskoningdhv.nl</a></div></div><!-- Header --><div class="header u-bgcolor-dark"><div class="u-container"><div class="header-logo"><img src="/Portals/mijn/Skins/klantportaal/assets/img/logo_subtitle.svg" alt="Pensioenfonds HaskoningDHV"><img src="/Portals/mijn/Skins/klantportaal/assets/img/logo_block.svg" alt="Pensioenfonds HaskoningDHV"></div></div></div><div id="ContentPane" runat="server"><div class="grid"><div class="DnnModule DnnModule-DNN_HTML DnnModule-2010"><a name="2010"></a><div class="tabcontent u-container"><div class="grid"><div class="col-span-12-sm col-span-8-md col-span-8-lg"><h1><span id="dnn_ctr2010_dnnTITLE_titleLabel" class="Head">Inloggen met DigiD</span></h1><div id="dnn_ctr2010_ContentPane"><!-- Start_Module_2010 --><div id="dnn_ctr2010_ModuleContent" class="DNNModuleContent ModDNNHTMLC"><div id="dnn_ctr2010_HtmlModule_lblContent" class="Normal"><p>Met MijnPensioenfondsHaskoningDHV heb je altijd inzicht in je actuele pensioensituatie.</p><p class="alert alert--warning" id="js-statusmessage">&nbsp;</p><p><img alt="Logo DidiD" class="logo-digid" height="40" src="/portals/mijn/logo_digid.png" title="Logo DidiD" width="40"><a class="button button--primary cui-digidlogin" data-js-id="digidlogin" href="/authentication/login">Inloggen met DigiD</a></p><ul class="linklist"><li><a href="https://www.digid.nl/" target="_blank"><svg><use xlink:href="/Portals/mijn/Skins/klantportaal/assets/icons/output/icons.symbol.svg#chevron-thin-right" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg>Vraag je DigiD aan</a></li><li><a href="https://www.digid.nl/vraag-en-antwoord" target="_blank"><svg><use xlink:href="/Portals/mijn/Skins/klantportaal/assets/icons/output/icons.symbol.svg#chevron-thin-right" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg>Veelgestelde vragen over DigiD</a></li></ul><p>Heb je een vraag over DigiD en staat deze niet bij de veelgestelde vragen? Neem dan contact op met de <a href="https://www.digid.nl" target="_blank">DigiD-helpdesk</a><a>.</a></p></div></div><!-- End_Module_2010 --></div></div></div></div></div></div></div></div><!-- Footer --><div class="footer u-bgcolor-dark"><div class="u-container"><a href="https://www.pensioenfondshaskoningdhv.nl"><img class="footer-primary-logo" src="/Portals/mijn/Skins/klantportaal/assets/img/logo.svg"><img class="footer-primary-logo" src="/Portals/mijn/Skins/klantportaal/assets/img/logo_block.svg"></a><div class="footer-address"><p class="footer-addressline address">Laan 1914 nr. 35 3818 EX Amersfoort<br>Postbus 1388 3800 BJ Amersfoort</p><p class="footer-addressline phone">(088) 348 2190</p><p class="footer-addressline email"><a href="mailto:pensioenfonds@rhdhv.com">pensioenfonds@rhdhv.com</a></p></div></div></div> </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
