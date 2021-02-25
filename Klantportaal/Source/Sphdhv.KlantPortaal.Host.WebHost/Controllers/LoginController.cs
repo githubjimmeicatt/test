6a2bc1223931c4000ae5c9b2a32587f5913e75f6
Pascal van Oostenbrugge <pascal@icatt.nl>
2021-02-25 16:12:49 +0100

    update configs | build moet falen

--- Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/Controllers/LoginController.cs
+++ Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/Controllers/LoginController.cs
@@ -11,7 +11,6 @@ namespace Sphdhv.KlantPortaal.Host.WebHost.Controllers
     public class LoginController : Controller
     {
         // Post
-        [AllowAnonymous]
         [System.Web.Http.HttpPost]
         public async System.Threading.Tasks.Task<ActionResult> Login(ImpersonateLoginModel loginModel)
         {
