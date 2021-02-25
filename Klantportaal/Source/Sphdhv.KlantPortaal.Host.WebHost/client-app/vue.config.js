1e42155c8c330d2f185725c2d1464096eb0bd2f3
Pascal van Oostenbrugge <pascal@icatt.nl>
2021-02-25 18:08:17 +0100

    geen source maps

--- Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/client-app/vue.config.js
+++ Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/client-app/vue.config.js
@@ -1,13 +1,14 @@
 ﻿module.exports = {
-      outputDir: 'dist',
-      chainWebpack: config => {
-            config.module
-                .rule('images')
-                .use('url-loader')
-                .loader('url-loader')
-                .tap(options => {
-                    options.limit = -1
-                    return options
-                })
-          },
+    outputDir: 'dist',
+    productionSourceMap: false,
+    chainWebpack: config => {
+        config.module
+            .rule('images')
+            .use('url-loader')
+            .loader('url-loader')
+            .tap(options => {
+                options.limit = -1
+                return options
+            })
+    }
 }
