6568669bf71355f39b0cf7eaca6c27b9f0047def
Armijn Teeuw <armijn@icatt.nl>
2022-11-07 15:19:49 +0100

    nieuws styling

--- Umbraco/DHV.Umbraco/client-app/src/components/LinkList.vue
+++ Umbraco/DHV.Umbraco/client-app/src/components/LinkList.vue
@@ -5,16 +5,17 @@
   >
     {{ title }}
   </h3>
+
   <ul
     v-if="links.length"
     class="row"
   >
     <li
-      v-for="({url, name}, i) in links"
+      v-for="({ url, name }, i) in links"
       :key="i"
     >
       <router-link
-        class="arrow-before link"
+        class="link"
         :to="url"
       >
         {{ name }}
@@ -41,61 +42,31 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/scss/_mixins.scss";
 
-h3{
-    margin-top: var(--space-medium);
-    font-variant: all-small-caps;
-    margin-bottom: calc(-1*var(--space-medium) + var(--space-small));
+h3 {
+  margin-top: var(--space-medium);
+  font-variant: all-small-caps;
+  margin-bottom: calc(-1 * var(--space-medium) + var(--space-small));
 }
 
-@include screen-fits-two-cards {
-    .row{
-        margin-left: auto;
-        margin-right: auto;
-        width: calc(2 *var(--card-width-large) + var(--space-medium));
-    }
-}
-
-@include screen-fits-three-cards {
-    .three, .four {
-    .row{
-        margin-left:0px;
-        margin-right:0px;
-        width: calc(3 *var(--card-width-small));
-    }
-    }
-}
-
-@include screen-fits-four-cards {
-    .four {
-    .row{
-        margin-left:0px;
-        margin-right:0px;
-        width: calc(4 *var(--card-width-small));
-    }
-    }
-}
-
-.row{
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    margin-top: var(--space-medium);
-    gap: var(--space-small);
-    padding:0px;
+.row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: var(--space-small);
+  padding: 0;
+  margin-block: var(--space-medium);
 }
 
 .row > li {
-    position:relative;
-    background-color: var(--color-base);
-    padding: var(--space-smallest) var(--space-small) var(--space-smallest) var(--space-small);
-    border-radius:20px;
-    flex-wrap: wrap;
+  font-weight: 600;
+  padding: var(--space-smaller) var(--space-small);
+  background-color: var(--color-base);
+  border-radius: 0.75rem;
 }
 
 .row .link {
-    text-decoration: none;
-    color: white;
+  text-decoration: none;
+  color: white;
 }
-
 </style>
