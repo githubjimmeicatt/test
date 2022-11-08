634495b96da86c32d41f9370891098c8f1f1ca53
Pascal van Oostenbrugge <pvanoostenbrugge@gmail.com>
2022-11-08 17:09:17 +0100

    page renames en sph homepage added

---
+++ Umbraco/DHV.Umbraco/client-app/src/pages/SphCorporateHome.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <page-header
+    v-bind="content.hero" />
+
+  <LiveEventCards
+    v-bind="content.cards" />
+
+  <TextNextToImage
+    v-bind="content.textNextToImage" />
+
+  <LatestNews
+    v-bind="content.newsPicker" />
+
+  <FormElement
+    v-bind="content.formulier" />
+
+</template>
+
+<script>
+import { inject } from 'vue'
+import LatestNews from '@/components/LatestNews.vue'
+import LiveEventCards from '@/components/LiveEventCards.vue'
+import TextNextToImage from '@/components/TextNextToImage.vue'
+import FormElement from '@/components/FormElement.vue'
+import PageHeader from '../components/PageHeader.vue'
+
+export default {
+  components: {
+    PageHeader,
+    LatestNews,
+    LiveEventCards,
+    TextNextToImage,
+    FormElement,
+  },
+  setup() {
+    const content = inject('content')
+    return {
+      content,
+    }
+  },
+
+}
+</script>
