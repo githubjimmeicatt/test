83a37ed2394d916bd3d61d732db70bf967f4a64e
Pascal van Oostenbrugge <pascal@icatt.nl>
2023-02-20 12:06:33 +0100

    Microsoft.CodeAnalysis.NetAnalyzer plus revert path traversal

--- Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/Controllers/ClientController.cs
+++ Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/Controllers/ClientController.cs
@@ -14,7 +14,9 @@ namespace Sphdhv.KlantPortaal.Host.WebHost.Controllers
                 throw new UnauthorizedAccessException("Not allowed to traverse parents");
             }
             var mappedPath = System.Web.Hosting.HostingEnvironment.MapPath("~/");
+#pragma warning disable SCS0018 // Potential Path Traversal vulnerability was found where '{0}' in '{1}' may be tainted by user-controlled data from '{2}' in method '{3}'.
             return System.IO.File.ReadAllText($@"{mappedPath}\StaticFiles\Client\Controller\{id}.js");
+#pragma warning restore SCS0018 // Potential Path Traversal vulnerability was found where '{0}' in '{1}' may be tainted by user-controlled data from '{2}' in method '{3}'.
         }
 
         // GET: Client
