a808636eb259adc7e9700f9dac6c295eafe518e0
Justin Alink <justin@icatt.nl>
2023-07-28 12:52:12 +0200

    removed last left-over javascript code

--- Umbraco/DHV.Umbraco/client-app/src/pages/NewsLetter.vue
+++ Umbraco/DHV.Umbraco/client-app/src/pages/NewsLetter.vue
@@ -24,63 +24,27 @@
 
   </section>
 
-  <Spinner v-if="isLoading" class="spinner" />
-
-  <Cards v-else title="Bekijk ook" :cards="otherNews" />
 </template>
 
 <script lang="ts">
-import { inject, computed } from 'vue'
+import { inject } from 'vue'
 
 import { useRoute } from 'vue-router'
-import { useUmbracoApi, useNewsCards, type NewsCard } from 'icatt-heartcore'
-import Cards from '@/components/Cards.vue'
-import Spinner from '@/assets/spinner.svg'
+import { useUmbracoApi } from 'icatt-heartcore'
 import RichText from '@/components/RichText.vue'
 import LazyImg from '@/components/LazyImg.vue'
 import NewsLetterTopicCards from '@/components/NewsLetterTopicCards.vue'
 import NewsLetterTopicCardDekkingsgraad from '@/components/NewsLetterTopicCardDekkingsgraad.vue'
-import { formatDate } from '@/helpers/formatDate'
 import Breadcrumbs from '@/components/Breadcrumbs.vue'
-
-function mapNewsItem({
-  summary, name, publishDate, url, image,
-}: NewsCard) {
-  return {
-    body: summary,
-    title: name,
-    date: publishDate,
-    subtitle: formatDate(publishDate),
-    image,
-    target: { url, name: 'Lees meer' },
-  }
-}
-
-function upOneLevel(path: string) {
-  const upperPath = path.replace(/\/$/, '').split('/')
-
-  if (upperPath.length > 0) {
-    upperPath.splice(upperPath.length - 1)
-    return upperPath.join('/')
-  }
-  return path
-}
-
-const maxItems = 3
 
 export default {
   components: {
-    RichText, LazyImg, Cards, Spinner, Breadcrumbs, NewsLetterTopicCards, NewsLetterTopicCardDekkingsgraad,
+    RichText, LazyImg, Breadcrumbs, NewsLetterTopicCards, NewsLetterTopicCardDekkingsgraad,
   },
 
   async setup() {
     const route = useRoute()
-    const parentPath = computed(() => upOneLevel(route.path))
     const content = inject<any>('content')
-    const parentId = computed(() => content.value?.parentId)
-    const { currentPage, isLoading } = useNewsCards(parentId, {
-      maxItems: maxItems + 1, // one more so we can exclude the current if necessary
-    })
 
     const newsLetterQuery = `{
   allNewsLetterArticleDetailPage(
@@ -157,18 +121,10 @@ export default {
       })),
     }
 
-    const otherNews = computed(() => currentPage.value.filter(({ id }) => id !== content.value?._id).slice(0, maxItems).map(mapNewsItem))
-
     return {
       content,
-      parentPath,
-      isLoading,
-      otherNews,
       items,
-      date: computed(() => {
-        const { publishDate, _createDate } = content.value ?? {}
-        return formatDate(publishDate) || formatDate(_createDate)
-      }),
+
     }
   },
 }
