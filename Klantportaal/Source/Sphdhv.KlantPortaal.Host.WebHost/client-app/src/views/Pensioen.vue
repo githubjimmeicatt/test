91d0efca4447345e2c22f39d7f4a40ecd686638d
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-03-04 17:07:12 +0100

    cleanup

--- Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/client-app/src/views/Pensioen.vue
+++ Klantportaal/Source/Sphdhv.KlantPortaal.Host.WebHost/client-app/src/views/Pensioen.vue
@@ -12,7 +12,7 @@
             <article>
                 <p>Tot voor kort stelden wij de tool PensioenPod beschikbaar om je te helpen meer inzicht in je financi&#235;le situatie bij pensionering te krijgen.</p>
                 <p>Omdat de tool niet aan de verwachtingen voldeed hebben we besloten de toegang tot deze tool van onze website te verwijderen.</p>
-                <p>We verwijzen je naar je UPO voor meer inzicht in je pensioen bij ons pensioenfonds en naar <a href="//www.mijnpensioenoverzicht.nl" target=”_blank”>www.mijnpensioenoverzicht.nl</a> voor meer inzicht in het totale pensioen dat je al opgebouwd hebt en het verwachte pensioenbedrag na pensionering, inclusief de AOW. En natuurlijk kan je altijd bij ons <a href="https://www.pensioenfondshaskoningdhv.nl/Home/Over-het-pensioenfonds/Pensioenbureau" target=”_blank”>Pensioenbureau</a> terecht voor vragen over je pensioen.</p>
+                <p>We verwijzen je naar je UPO voor meer inzicht in je pensioen bij ons pensioenfonds en naar <a href="//www.mijnpensioenoverzicht.nl" target="_blank">www.mijnpensioenoverzicht.nl</a> voor meer inzicht in het totale pensioen dat je al opgebouwd hebt en het verwachte pensioenbedrag na pensionering, inclusief de AOW. En natuurlijk kan je altijd bij ons <a href="https://www.pensioenfondshaskoningdhv.nl/Home/Over-het-pensioenfonds/Pensioenbureau" target="_blank">Pensioenbureau</a> terecht voor vragen over je pensioen.</p>
             </article>
 
         </div>
